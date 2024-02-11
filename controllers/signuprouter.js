3c4a8796b0e3e7c0d7c620f0806b568d0e1f3db3
Annateresa01 <shajianna013@gmail.com>
2024-02-11 16:05:47 +0530

    ok

--- controllers/signuprouter.js
+++ controllers/signuprouter.js
@@ -12,14 +12,21 @@ hashpasswordgenerator=async(pass)=>{
 router.post("/signup",async(req,res)=>{
     let {data}={data:req.body}
     let password=data.userpassword
-    
+    hashpasswordgenerator(password).then((hashedpassword)=>
+    {
+        data.userpassword=hashedpassword
+        console.log(data)
+          
     let resumes=new resumerouter(data)
-    let response=await resumes.save()
+    let response=resumes.save()
     res.json(
         {
             status:"Success"
         }
     )
+
+    })
+    
 
     
 })
